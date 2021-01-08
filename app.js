b8508a41b7e076f69780652adc67821aebc99133
Daniel Szczotka <daniel.szczotka10@gmail.com>
2021-01-08 11:12:55 +0100

    Add gitignore, procfile and working ports

--- app.js
+++ app.js
@@ -132,6 +132,11 @@ app.post("/deleteTasks", function(req, res){
     });
 });
 
-app.listen(3000, function(){
-  console.log("Server listen on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+  console.log("Server started successfully");
 });
